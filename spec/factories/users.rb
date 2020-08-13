9cb0634243b625baa3fe2573e89fbe9b538a98e3
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-13 18:17:45 +0900

    factory_bot & Faker の導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"yamada"}
+    email                 {"aaaaaaa@gmail.com"}
+    password              {"1234567"}
+    password_confirmation {password}
+    first_name            {"山田"}
+    family_name           {"太郎"}
+    first_name_kana       {"ヤマダ"}
+    family_name_kana      {"タロウ"}
+    birth_date            {"1980-1-1"}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "emailが空では登録できない" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+  end
+end
