716003a2796332d28d9fbec7433f207cfb0c369d
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-13 17:47:39 +0900

    ログイン機能とバリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_kana, :family_name_kana,:birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,:family_name,:first_name_kana,:family_name_kana,:birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true, uniqueness: true, inclusion: { in: ["@"] }
+         validates :password, presence: true, confirmation: true, format: {with: /[a-z\d]{6,}/}
+         validates :password_confirmation, presence: true
+         validates :first_name, :family_name, presence: true, format: {with: /\A[あ-んア-ン一-龥]/}
+         validates :first_name_kana, :family_name_kana, presence: true, format: {with: /\A[ア-ン]+\z/}
+         validates :birth_date, presence: true
+
 end
+
+         
