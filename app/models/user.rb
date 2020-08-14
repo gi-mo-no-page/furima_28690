9ad4d902eed695179339185ce2c1e200b1924430
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-14 11:45:30 +0900

    テスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :email, presence: true, uniqueness: true, inclusion: { in: ["@"] }
-         validates :password, presence: true, confirmation: true, format: {with: /[a-z\d]{6,}/}
+         validates :email, presence: true, uniqueness: true, format: {with: /\A(?=.*@).*\z/}
+         validates :password, presence: true, confirmation: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/}
          validates :password_confirmation, presence: true
-         validates :first_name, :family_name, presence: true, format: {with: /\A[あ-んア-ン一-龥]/}
-         validates :first_name_kana, :family_name_kana, presence: true, format: {with: /\A[ア-ン]+\z/}
+         validates :first_name, :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :first_name_kana, :family_name_kana, presence: true, format: {with: /\A[ァ-ンー－]+\z/}
          validates :birth_date, presence: true
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {"yamada"}
     email                 {"aaaaaaa@gmail.com"}
-    password              {"1234567"}
+    password              {"123yamada"}
     password_confirmation {password}
     first_name            {"山田"}
     family_name           {"太郎"}
