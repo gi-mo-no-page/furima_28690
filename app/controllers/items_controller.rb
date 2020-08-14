c602bc340e86368200a31ab71fed9103345541cf
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-14 17:39:20 +0900

    Active_hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,27 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+  end
+
+  def create
+    @item = Item.create(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+
+  private
+   def item_params
+    params.require(:message).permit(:name, :image).merge(user_id: current_user.id)
+   end
+
 
   def move_to_index
     unless user_signed_in?
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  has_one_attached :image
+  belongs_to :user
+
+  validates :category, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
 end
