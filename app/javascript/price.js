488818d0a2cc7be69b3cda317e18f9d712391a71
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-16 07:36:44 +0900

    値段の表示

---
+++ app/javascript/price.js
@@ -0,0 +1,10 @@
+function input () {
+  const price = document.getElementById('item-price')
+  const tax = document.getElementById('add-tax-price')
+  const profit = document.getElementById('profit')
+  price.addEventListener('input', ()=>{
+    tax.textContent = Math.floor(price.value / 10);
+    profit.textContent = Math.floor(price.value - tax.textContent);
+  })
+}
+window.addEventListener("load", input);
