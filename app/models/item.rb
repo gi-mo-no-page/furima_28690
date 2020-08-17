615c74a2608b08b456b1d573b40240dae5ed0659
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-17 11:36:18 +0900

    テスト完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :shopping_charge
   belongs_to_active_hash :shopping_region
   belongs_to_active_hash :delivery_time
 
-  has_one_attached :image
-  belongs_to :user
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :status_id
+    validates :shopping_charges_id
+    validates :shopping_region_id
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 
-  validates :category, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
+  
+  
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   {"写真"}
+    introduction           {"写真です"}
+    category_id            {"2"}
+    status_id              {"2"}
+    shopping_charges_id    {"1"}
+    shopping_region_id     {"1"}
+    price                  {"10000"} 
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open("spec/images/test.jpg"), filename: "test.jpg", content_type: 'image/jpg')
+    end
   end
 end
+
