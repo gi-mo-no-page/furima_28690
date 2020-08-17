ba483228b25ec34c3e527d55b23824600094be4e
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-17 22:44:29 +0900

    編集機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])    
   end
 
+  def edit
+    @item = Item.find(params[:id])  
+  end
+
+  def update
+     item = Item.find(params[:id])
+     item.update(item_params)
+      if item.save
+        redirect_to root_path
+      else
+        render :edit
+      end
+  end
+
 
   private
    def item_params
