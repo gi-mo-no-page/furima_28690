a6ea54ad2d3001051347a1248beb83392af30282
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-20 16:53:22 +0900

    gem rails_12factorの追加

--- Gemfile
+++ Gemfile
@@ -68,3 +68,7 @@ gem 'image_processing', '~> 1.2'
 gem 'active_hash'
 
 gem 'payjp'
+
+group :production do
+  gem 'rails_12factor'
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
-
-  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,:family_name,:first_name_kana,:family_name_kana,:birth_date])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変
+    end
+  end
+
 end
