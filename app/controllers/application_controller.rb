6a3d77921bace263d826b6b88dde7bf77e558109
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-20 16:36:49 +0900

    調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name,:family_name,:first_name_kana,:family_name_kana,:birth_date])
