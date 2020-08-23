3879df75aa6704b1e91a3c054e01ceb2dbdc6ef4
gi-mo-no-page <gi-mo-no-page@users.noreply.github.com>
2020-08-23 21:35:13 +0900

    ユーザーマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'items#index'
+  resources :users, only: [:show, :update]
   resources :items do
     resources :purchases, only:[:index, :create]
   end
